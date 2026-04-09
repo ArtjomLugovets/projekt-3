d052a78009d6b18e9b70522bf957e54fa7146979
Artjom L <artjom.lugovets@techno.ee>
2026-04-09 14:09:51 +0300

    Added players

--- script.js
+++ script.js
@@ -1,16 +1,89 @@
 const world = document.getElementById("world");
 
 async function loadWorld() {
-  const res = await fetch("/sdb/poly/wander");
-  const data = await res.json();
+  const response = await fetch("https://tinkr.tech/sdb/Artjom/wanderworld");
+  const data = await response.json();
 
-  console.log(data)
+  render(data);
+}
 
-  for (const player of data.players) {
+function render(state) {
+  world.innerHTML = "";
+
+  for (const player of state.players) {
     const div = document.createElement("div");
-    div.textContent = player.username;
+    div.className = "player";
+
+    div.style.left = player.x + "px";
+    div.style.top = player.y + "px";
+
+    if (player.message) {
+      const bubble = document.createElement("div");
+      bubble.className = "bubble";
+      bubble.textContent = player.message;
+      div.appendChild(bubble);
+    }
+
+    const img = document.createElement("img");
+    img.src = player.image;
+    div.appendChild(img);
+
+    const name = document.createElement("div");
+    name.textContent = player.username;
+    div.appendChild(name);
+
     world.appendChild(div);
+
   }
 }
 
+async function join() {
+  const response = await fetch("https://tinkr.tech/sdb/Artjom/wanderworld", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      action: "join",
+      username: "Artjom"
+    })
+  });
+
+  const data = await response.json();
+
+  localStorage.setItem("key", data.player_key);
+}
+
+world.addEventListener("click", (e) => {
+  fetch("https://tinkr.tech/sdb/Artjom/wanderworld", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      action: "move",
+      player_key: localStorage.getItem("key"),
+      x: e.offsetX,
+      y: e.offsetY
+    })
+  });
+
+  loadWorld();
+})
+
+async function talk(text) {
+  const response = await fetch("https://tinkr.tech/sdb/Artjom/wanderworld", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      action: "talk",
+      player_key: localStorage.getItem("key"),
+      message: text
+    })
+  });
+}
+console.log(localStorage.getItem("key"));
+join();
 loadWorld();
